17306ff968ad09835a612dc3caca540b47f76a96
Galcian79 <38818634+Galcian79@users.noreply.github.com>
2024-10-03 18:22:06 +0200

    Add files via upload
    Ryujinx building script for (Arch)linux.

---
+++ build.sh
@@ -0,0 +1,38 @@
+###   Ryujinx - Nintendo Switch Emulator - Building Script
+
+# Project - https://git.frfrnocap.men/ryujinx-archive
+
+
+# Makedepends for Archlinux (other distros may vary):  'desktop-file-utils' 'dotnet-sdk-bin' 'jq'
+
+
+  export DOTNET_CLI_TELEMETRY_OPTOUT=1
+
+  dotnet clean
+  dotnet nuget locals all -c
+
+  _args=(
+    -c Release
+    -r linux-x64
+    --nologo
+    --self-contained true
+    -p:DebugType=none
+    -p:ExtraDefineConstants=DISABLE_UPDATER
+    -p:Version=1.1-master
+  )
+
+  echo "Building AVA Interface..."
+  dotnet publish "${_args[@]}" -o publish_ava src/Ryujinx
+
+  echo "Building GTK3 Interface..."
+  dotnet publish "${_args[@]}" -o publish_gtk src/Ryujinx.Gtk3
+
+  echo "Building SDL2 Headless..."
+  dotnet publish "${_args[@]}" -o publish_sdl src/Ryujinx.Headless.SDL2
+
+  echo "Shutting down dotnet build server in background."
+  (timeout -k 45 30 dotnet build-server shutdown) > /dev/null 2>&1 &
+
+
+
+### extra credits to Galcian79 - https://github.com/Galcian79/didactic-adventure
